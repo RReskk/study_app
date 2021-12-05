b33e986f358c2084cf10f4eb12857960d801820e
RReskk <zyxorus@mail.ru>
2021-12-05 18:47:32 +0500

    initial

---
+++ app/src/main/java/jk/freedws/study/MainActivity.java
@@ -0,0 +1,38 @@
+package jk.freedws.study;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView action_bar_text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.action_bar);
+        setContentView(R.layout.activity_main);
+        action_bar_text = findViewById(R.id.actionbar_title);
+        action_bar_text.setText("Обществознание");
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_menu);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+    }
+}
