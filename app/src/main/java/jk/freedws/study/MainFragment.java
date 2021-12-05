bbb91970e5235ffd4317208f32c2e2784db9496e
RReskk <zyxorus@mail.ru>
2021-12-05 22:20:29 +0500

    db added

--- app/src/main/java/jk/freedws/study/MainFragment.java
+++ app/src/main/java/jk/freedws/study/MainFragment.java
@@ -3,6 +3,7 @@ package jk.freedws.study;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,9 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class MainFragment extends Fragment {
+
+    RecyclerView recyclerView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
