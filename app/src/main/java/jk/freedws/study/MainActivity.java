bc60bdc0bb55a0ed845933dc135d5378a91eaa03
RReskk <zyxorus@mail.ru>
2021-12-06 12:22:36 +0500

    done build

--- app/src/main/java/jk/freedws/study/MainActivity.java
+++ app/src/main/java/jk/freedws/study/MainActivity.java
@@ -12,12 +12,17 @@ import androidx.navigation.ui.NavigationUI;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEvent;
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,10 +33,25 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.action_bar);
         setContentView(R.layout.activity_main);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_menu);
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
         NavController navController = navHostFragment.getNavController();
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+
+        KeyboardVisibilityEvent.setEventListener(
+                this,
+                new KeyboardVisibilityEventListener() {
+                    @Override
+                    public void onVisibilityChanged(boolean isOpen) {
+                        if(isOpen){
+                            bottomNavigationView.setVisibility(View.INVISIBLE);
+                        }else{
+                            bottomNavigationView.setVisibility(View.VISIBLE);
+                        }
+                    }
+                });
     }
 }
